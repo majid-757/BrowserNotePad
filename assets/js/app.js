9e83686b11ea74b69d4771a05295f14e21b50d6b
majid <majid.asadollahi95@gmail.com>
2023-02-07 23:45:55 +0330

    remove X in notes

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,6 +76,9 @@ function eventLisnener(e) {
 
     // remove note
     document.querySelector('#note-list').addEventListener('click', removeNote)
+
+    // get data from localstorage on loaded
+    document.addEventListener('DOMContentLoaded', localStorageOnLoad)
 }
 
 
@@ -94,14 +97,88 @@ function newNote(e) {
     li.appendChild(removeBtn)
     noteList.appendChild(li)
 
+    addNoteToLocalStorage(note)
 
 }
 
 
+
 function removeNote(e) {
-    if(e.target.classList.contains('remove-note')){
+    if(e.target.classList.contains('remove-note')) {
         e.target.parentElement.remove()
+    }
+
+    // also remove the note from the local storag
+    removeNoteLocalStorage(e.target.parentElement.textContent)
+
+}
+
+
+// adding note to the local storage
+function addNoteToLocalStorage(note) {
+
+    // get the from localstorage
+    const notes = getNotesFromLocalStorage()
+
+    // add new note to the notes array  
+    notes.push(note)
+
+    // add new notes array to the localstorage
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+
+}
+
+
+
+
+
+// get notes from localstorage
+function getNotesFromLocalStorage() {
+    let notes;
+
+    // get previous notes from localstorage
+    let getFormLS = localStorage.getItem('notes');
+
+    // if not exist create empty array 
+    if(getFormLS === null) {
+        notes = []
+    } else {
+        // if exist convert to the array 
+        notes = JSON.parse(getFormLS)
     } 
+    
+    return notes
+}
+
+
+//get data from local storage on load
+function localStorageOnLoad() {
+    const  notes = getNotesFromLocalStorage()
+
+    // print each item of array
+    notes.forEach(function(note) {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(note))
+
+        const removeBtn = document.createElement('a')
+        removeBtn.textContent = 'X'
+        removeBtn.classList = 'remove-note'
+
+        li.appendChild(removeBtn)
+        noteList.appendChild(li)
+    });
+
+}
+
+
+// also remove note from localstorage
+function removeNoteLocalStorage(noteContent) {
+
+    // delete x from the content
+    const  note = noteContent.substring(0, noteContent.length - 1) 
+
+    console.log(note)
 }
 
 
